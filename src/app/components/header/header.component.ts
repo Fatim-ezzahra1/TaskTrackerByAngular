9df5543e71dd321d4d58d8c4cfc9f88b9b2ddbf2
Fatim-Ezzahra <aitomgharfatimezzahra@gmail.com>
2025-04-09 21:49:40 +0100

    Initial commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [ButtonComponent, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  title: string = 'Task tracker';
+  showAddTask: boolean=false;
+  subscription?: Subscription;
+
+  constructor(private uiService : UiService, private router: Router){
+    this.subscription=this.uiService
+    .onToggle()
+    .subscribe((value)=>{
+      this.showAddTask = value;
+    });
+  }
+
+  hasRoute(route:string){
+    return this.router.url === route;
+  }
+
+  toggleAddTask(){
+    this.uiService.toggleAddTask();
+  }
+}
